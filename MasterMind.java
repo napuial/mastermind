a438c0d64a4fc81ac8b947d206de2ad092ec9d01
napuial <125679202+napuial@users.noreply.github.com>
2024-11-22 14:37:58 +0100

    Dzień 4 - LiveCoding

--- MasterMind.java
+++ MasterMind.java
@@ -1,23 +1,39 @@
+import java.util.Scanner;
 import java.util.Random;
 
 public class MasterMind {
     public static void main(String[] args) {
         System.out.println("Witaj w grze MasterMind!");
 
+        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
         int accessCodeLength = 4;
         int maxCodeValue = 6;
         int[] accessCode = new int[accessCodeLength];
+        int[] userCode = new int[accessCodeLength];
 
         for(int i = 0; i < accessCodeLength; i++) {
             accessCode[i] = random.nextInt(maxCodeValue) + 1;
         }
 
-        System.out.println("Kod dostępu to:");
+        System.out.println("Wprowadż kod dostępu: ");
+        String input = scanner.nextLine();
 
-        for(int i = 0; i < accessCodeLength; i++) {
-            System.out.print(accessCode[i]);
+        try {
+            if(input.length() != accessCodeLength) {
+                throw new NumberFormatException();
+            }
+            for(int i = 0; i < accessCodeLength; i++) {
+                userCode[i] = Character.getNumericValue(input.charAt(i));
+                if(userCode[i] < 1 || userCode[i] > maxCodeValue) {
+                    throw new NumberFormatException();
+                }
+            }
+        } catch(NumberFormatException e) {
+            System.out.println("Przegrana. Zapoznaj się z zasadami gry i spróbuj ponownie.");
         }
+
+        scanner.close();
     }
 }
