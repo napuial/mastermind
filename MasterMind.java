5760c4d7ac6f8aee0a62a407495e34acfd354fff
napuial <125679202+napuial@users.noreply.github.com>
2024-11-22 15:56:43 +0100

    Dzień 5 - LiveCoding

--- MasterMind.java
+++ MasterMind.java
@@ -12,26 +12,62 @@ public class MasterMind {
         int maxCodeValue = 6;
         int[] accessCode = new int[accessCodeLength];
         int[] userCode = new int[accessCodeLength];
+        boolean isUserCodeCorrect = false;
 
         for(int i = 0; i < accessCodeLength; i++) {
             accessCode[i] = random.nextInt(maxCodeValue) + 1;
         }
 
-        System.out.println("Wprowadż kod dostępu: ");
-        String input = scanner.nextLine();
+        while(isUserCodeCorrect == false) {
+            System.out.println("Wprowadź kod dostępu: ");
+            String input = scanner.nextLine();
 
-        try {
-            if(input.length() != accessCodeLength) {
-                throw new NumberFormatException();
-            }
-            for(int i = 0; i < accessCodeLength; i++) {
-                userCode[i] = Character.getNumericValue(input.charAt(i));
-                if(userCode[i] < 1 || userCode[i] > maxCodeValue) {
+            try {
+                if(input.length() != accessCodeLength) {
                     throw new NumberFormatException();
                 }
+                for(int i = 0; i < accessCodeLength; i++) {
+                    userCode[i] = Character.getNumericValue(input.charAt(i));
+                    if(userCode[i] < 1 || userCode[i] > maxCodeValue) {
+                        throw new NumberFormatException();
+                    }
+                }
+            } catch(NumberFormatException e) {
+                System.out.println("Zapoznaj się z zasadami gry i spróbuj ponownie.");
             }
-        } catch(NumberFormatException e) {
-            System.out.println("Przegrana. Zapoznaj się z zasadami gry i spróbuj ponownie.");
+
+            int correctValuesInRightPlaces = 0;
+            int correctValuesInWrongPlaces = 0;
+            boolean[] accessCodeVerificationTable = new boolean[accessCodeLength];
+            boolean[] userCodeVerificationTable = new boolean[accessCodeLength];
+
+            for(int i = 0; i < accessCodeLength; i++) {
+                if(userCode[i] == accessCode[i]) {
+                    correctValuesInRightPlaces++;
+                    userCodeVerificationTable[i] = true;
+                    accessCodeVerificationTable[i] = true;
+                }
+            }
+
+            for(int i = 0; i < accessCodeLength; i++) {
+                if(userCodeVerificationTable[i] != true) {
+                    for(int j = 0; j < accessCodeLength; j++) {
+                        if(accessCodeVerificationTable[j] == false && userCode[i] == accessCode[j]) {
+                            correctValuesInWrongPlaces++;
+                            userCodeVerificationTable[i] = true;
+                            accessCodeVerificationTable[i] = true;
+                        }
+                    }
+                }
+            }
+
+            if(correctValuesInRightPlaces == accessCodeLength) {
+                System.out.println("Uzyskano dostęp. Gratulacje.");
+                isUserCodeCorrect = true;
+            } else {
+                System.out.println("Poprawne wartości na poprawnych pozycjach: " + correctValuesInRightPlaces);
+                System.out.println("Poprawne wartości na niepoprawnych pozycjach: " + correctValuesInWrongPlaces);
+            }
         }
 
         scanner.close();
